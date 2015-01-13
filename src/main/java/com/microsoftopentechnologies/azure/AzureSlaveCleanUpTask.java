329b2e9129e9f8906e53df766eb0c42b01ee5f5a
jyrmyx <jyri.ilama@gmail.com>
2015-01-13 09:07:18 +0200

    Fix forever-loop of already deleted slaves.

--- src/main/java/com/microsoftopentechnologies/azure/AzureSlaveCleanUpTask.java
+++ src/main/java/com/microsoftopentechnologies/azure/AzureSlaveCleanUpTask.java
@@ -60,9 +60,14 @@ public final class AzureSlaveCleanUpTask extends AsyncPeriodicWork {
 							} catch (Exception e) {
 								retryCount++;
 								LOGGER.info("AzureSlaveCleanUpTask: execute: Exception occured while calling timeout on node , \n"
-											+ "Will retry again after 30 seconds. Current retry count "+retryCount + "\n"
+											+ "Will retry again after 30 seconds. Current retry count "+retryCount + " / 30\n"
 											+ "Error code "+e.getMessage());
 								// We won't get exception for RNF , so for other exception types we can retry
+								if (e.getMessage().contains("not found in the currently deployed service")) {
+									LOGGER.info("AzureSlaveCleanUpTask: execute: Slave does not exist in the subscription anymore, setting shutdownOnIdle to True");
+									slaveNode.setShutdownOnIdle(true);
+									break;
+								}
 								try {
 									Thread.sleep(30 * 1000);
 								} catch (InterruptedException e1) {
