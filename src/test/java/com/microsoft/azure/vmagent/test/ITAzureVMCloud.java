7a2ca4701f130b739bcdbb31b73471f16639c13e
Claudiu Guiman <clguiman@microsoft.com>
2016-12-20 15:56:48 -0800

    Add integration tests

---
+++ src/test/java/com/microsoft/azure/vmagent/test/ITAzureVMCloud.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2017.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.azure.vmagent.test;
+
+import com.microsoft.azure.vmagent.AzureVMAgent;
+import com.microsoft.azure.vmagent.AzureVMAgentTemplate;
+import com.microsoft.azure.vmagent.AzureVMCloud;
+import com.microsoft.azure.vmagent.AzureVMDeploymentInfo;
+import com.microsoft.azure.vmagent.exceptions.AzureCloudException;
+import com.microsoft.azure.vmagent.util.Constants;
+import hudson.model.Node;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class ITAzureVMCloud extends IntegrationTest {
+
+    private static final Logger LOGGER = Logger.getLogger(ITAzureVMCloud.class.getName());
+
+    @Test
+    public void createProvisionedAgentThowsExceptionWhenNoDeployments() {
+        try {
+            final String vmName = "fakeVM";
+            final String deploymentName = "fakeDeployment";
+            AzureVMAgentTemplate templateMock = mock(AzureVMAgentTemplate.class);
+            AzureVMCloud cloudMock = spy( new AzureVMCloud(servicePrincipal, "xyz", "42", "0", testEnv.azureResourceGroup, null));
+
+            when(templateMock.getAzureCloud()).thenReturn(cloudMock);
+            when(cloudMock.getServicePrincipal()).thenReturn(servicePrincipal);
+
+            try {
+                cloudMock.createProvisionedAgent(templateMock, vmName, deploymentName);
+                Assert.assertTrue(false);
+            } catch (AzureCloudException ex) {
+                Assert.assertTrue(true);
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, null, e);
+            Assert.assertTrue(e.getMessage(), false);
+        }
+    }
+
+    @Ignore
+    @Test
+    public void createProvisionedAgentWorksWhenDeploymentExists() {
+        try {
+            final AzureVMDeploymentInfo deploymentInfo = createDefaultDeployment(1, null);
+            final String vmName = deploymentInfo.getVmBaseName() + "0";
+            final String vmDNS = customTokenCache.getAzureClient().virtualMachines().getByGroup(testEnv.azureResourceGroup, vmName).getPrimaryPublicIpAddress().fqdn();
+            final String deploymentName = deploymentInfo.getDeploymentName();
+            final String templateName = "createTemplate";
+            final String templateDesc = "createTemplateDesc";
+            final String agentWorkspace = "createTemplateAgentWorkspace";
+            final int noOfParallelJobs = 2;
+            final Node.Mode useAgentAlwaysIfAvail = Node.Mode.NORMAL;
+            final String templateLabels = "label1 label2";
+            final String credentialsId = "cred_id";
+            final String jvmOptions = "";
+            final boolean isShutdownOnIdle = false;
+            final int retentionTimeInMin = 30;
+            final String initScript = "";
+            final String agentLaunchMethod = Constants.LAUNCH_METHOD_SSH;
+            final boolean executeInitScriptAsRoot = true;
+            final boolean doNotUseMachineIfInitFails = true;
+
+            AzureVMAgentTemplate templateMock = mock(AzureVMAgentTemplate.class);
+            AzureVMCloud cloudMock = spy( new AzureVMCloud(servicePrincipal, credentialsId, "42", "30", testEnv.azureResourceGroup, null));
+
+            when(cloudMock.getServicePrincipal()).thenReturn(servicePrincipal);
+            when(templateMock.getAzureCloud()).thenReturn(cloudMock);
+            when(templateMock.getTemplateName()).thenReturn(templateName);
+            when(templateMock.getTemplateDesc()).thenReturn(templateDesc);
+            when(templateMock.getAgentWorkspace()).thenReturn(agentWorkspace);
+            when(templateMock.getNoOfParallelJobs()).thenReturn(noOfParallelJobs);
+            when(templateMock.getUseAgentAlwaysIfAvail()).thenReturn(useAgentAlwaysIfAvail);
+            when(templateMock.getLabels()).thenReturn(templateLabels);
+            when(templateMock.getCredentialsId()).thenReturn(credentialsId);
+            when(templateMock.getJvmOptions()).thenReturn(jvmOptions);
+            when(templateMock.isShutdownOnIdle()).thenReturn(isShutdownOnIdle);
+            when(templateMock.getRetentionTimeInMin()).thenReturn(retentionTimeInMin);
+            when(templateMock.getInitScript()).thenReturn(initScript);
+            when(templateMock.getAgentLaunchMethod()).thenReturn(agentLaunchMethod);
+            when(templateMock.getResourceGroupName()).thenReturn(testEnv.azureResourceGroup);
+            when(templateMock.getExecuteInitScriptAsRoot()).thenReturn(executeInitScriptAsRoot);
+            when(templateMock.getDoNotUseMachineIfInitFails()).thenReturn(doNotUseMachineIfInitFails);
+
+            AzureVMAgent newAgent = cloudMock.createProvisionedAgent(templateMock, vmName, deploymentName);
+
+            Assert.assertEquals(vmDNS, newAgent.getPublicDNSName());
+            Assert.assertEquals(Constants.DEFAULT_SSH_PORT, newAgent.getSshPort());
+            Assert.assertEquals(templateName, newAgent.getTemplateName());
+            Assert.assertEquals(templateDesc, newAgent.getNodeDescription());
+            Assert.assertEquals(noOfParallelJobs, newAgent.getNumExecutors());
+            Assert.assertEquals(useAgentAlwaysIfAvail, newAgent.getMode());
+            Assert.assertEquals(templateLabels, newAgent.getLabelString());
+            Assert.assertEquals(jvmOptions, newAgent.getJvmOptions());
+            Assert.assertEquals(retentionTimeInMin, newAgent.getRetentionTimeInMin());
+            Assert.assertEquals(initScript, newAgent.getInitScript());
+            Assert.assertEquals(agentLaunchMethod, newAgent.getAgentLaunchMethod());
+            Assert.assertEquals(executeInitScriptAsRoot, newAgent.getExecuteInitScriptAsRoot());
+            Assert.assertEquals(doNotUseMachineIfInitFails, newAgent.getDoNotUseMachineIfInitFails());
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, null, e);
+            Assert.assertTrue(e.getMessage(), false);
+        }
+    }
+
+}
---
+++ src/test/java/com/microsoft/azure/vmagent/test/IntegrationTest.java
@@ -0,0 +1,237 @@
+/*
+ Copyright 2016 Microsoft, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+package com.microsoft.azure.vmagent.test;
+
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.microsoft.azure.CloudException;
+import com.microsoft.azure.management.compute.KnownLinuxVirtualMachineImage;
+import com.microsoft.azure.management.compute.VirtualMachine;
+import com.microsoft.azure.management.storage.StorageAccountKey;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageCredentialsAccountAndKey;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.core.PathUtility;
+import com.microsoft.azure.util.AzureCredentials.ServicePrincipal;
+import com.microsoft.azure.vmagent.AzureVMAgentCleanUpTask;
+import com.microsoft.azure.vmagent.AzureVMAgentTemplate;
+import com.microsoft.azure.vmagent.AzureVMCloud;
+import com.microsoft.azure.vmagent.AzureVMDeploymentInfo;
+import com.microsoft.azure.vmagent.AzureVMManagementServiceDelegate;
+import com.microsoft.azure.vmagent.exceptions.AzureCloudException;
+import com.microsoft.azure.vmagent.util.Constants;
+import com.microsoft.azure.vmagent.util.TokenCache;
+import hudson.util.Secret;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.*;
+import org.junit.rules.Timeout;
+import org.jvnet.hudson.test.JenkinsRule;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/*
+To execute the integration tests you need to set the credentials env variables (the ones that don't have a default) and run mvn failsafe:integration-test
+*/
+public class IntegrationTest {
+    @ClassRule public static JenkinsRule j = new JenkinsRule();
+    @Rule public Timeout globalTimeout = Timeout.seconds(20 * 60); // integration tests are very slow
+    private static final Logger LOGGER = Logger.getLogger(IntegrationTest.class.getName());
+    
+
+    protected static class TestEnvironment {
+        public final String subscriptionId;
+        public final String clientId;
+        public final String clientSecret;
+        public final String oauth2TokenEndpoint;
+        public final String serviceManagementURL;
+        public final String authenticationEndpoint;
+        public final String resourceManagerEndpoint;
+        public final String graphEndpoint;
+        public final String azureLocation;
+        public final String azureResourceGroup;
+        public final String azureStorageAccountName;
+        public final String azureImagePublisher;
+        public final String azureImageOffer;
+        public final String azureImageSku;
+        public final String azureImageSize;
+
+        TestEnvironment() {
+            subscriptionId = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_SUBSCRIPTION_ID");
+            clientId = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_CLIENT_ID");
+            clientSecret = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_CLIENT_SECRET");
+            oauth2TokenEndpoint = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_TOKEN_ENDPOINT");
+            serviceManagementURL = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_AZURE_URL");
+            authenticationEndpoint = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_AZURE_AUTH_URL");
+            resourceManagerEndpoint = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_AZURE_RESOURCE_URL");
+            graphEndpoint = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_AZURE_GRAPH_URL");
+
+            azureLocation = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_LOCATION", "East US");
+            azureResourceGroup = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_RESOURCE_GROUP_PREFIX", "vmagents-tst") + "-" + TestEnvironment.GenerateRandomString(16);
+            azureStorageAccountName = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_STORAGE_NAME_PREFIX", "vmtst") + TestEnvironment.GenerateRandomString(19);
+            azureImagePublisher = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_IMAGE_PUBLISHER", "Canonical");
+            azureImageOffer = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_IMAGE_OFFER", "UbuntuServer");
+            azureImageSku = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_IMAGE_SKU", "14.04.5-LTS");
+            azureImageSize = TestEnvironment.loadFromEnv("VM_AGENTS_TEST_DEFAULT_IMAGE_SIZE", "Basic_A0");
+        }
+
+        private static String loadFromEnv(final String name) {
+            return TestEnvironment.loadFromEnv(name, "");
+        }
+
+        private static String loadFromEnv(final String name, final String defaultValue)
+        {
+            final String value = System.getenv(name);
+            if (value == null || value.isEmpty()) {
+                return defaultValue;
+            } else {
+                return value;
+            }
+        }
+
+        public static String GenerateRandomString(int length) {
+            String uuid = UUID.randomUUID().toString();
+            return uuid.replaceAll("[^a-z0-9]","a").substring(0, length);
+        }
+    }
+
+    protected TokenCache customTokenCache = null;
+    protected TestEnvironment testEnv = null;
+    protected ServicePrincipal servicePrincipal = null;
+
+    @Before
+    public void setUp() {
+        testEnv = new TestEnvironment();
+        LOGGER.log(Level.INFO, "=========================== {0}", testEnv.azureResourceGroup);
+        servicePrincipal = new ServicePrincipal(
+            testEnv.subscriptionId,
+            testEnv.clientId,
+            testEnv.clientSecret,
+            testEnv.oauth2TokenEndpoint,
+            testEnv.serviceManagementURL,
+            testEnv.authenticationEndpoint,
+            testEnv.resourceManagerEndpoint,
+            testEnv.graphEndpoint);
+        customTokenCache = TokenCache.getInstance(servicePrincipal);
+        clearAzureResources();
+    }
+
+    @After
+    public void tearDown() {
+        clearAzureResources();
+    }
+
+    protected void clearAzureResources() {
+        try {
+            customTokenCache.getAzureClient().resourceGroups().deleteByName(testEnv.azureResourceGroup);
+        } catch (CloudException e) {
+            if (e.getResponse().code() != 404) {
+                LOGGER.log(Level.SEVERE, null, e);
+            }
+        }
+        
+    }
+
+    protected String downloadFromAzure(final String resourceGroup, final String storageAccountName, final String containerName, final String fileName) throws URISyntaxException, StorageException, IOException {
+        List<StorageAccountKey> storageKeys = customTokenCache.getAzureClient().storageAccounts().getByGroup(resourceGroup, storageAccountName).getKeys();
+        String storageAccountKey = storageKeys.get(0).value();
+        CloudStorageAccount account = new CloudStorageAccount(new StorageCredentialsAccountAndKey(storageAccountName, storageAccountKey));
+        CloudBlobClient blobClient = account.createCloudBlobClient();
+        CloudBlobContainer container = blobClient.getContainerReference(containerName);
+        CloudBlockBlob blob = container.getBlockBlobReference(fileName);
+        return blob.downloadText();
+    }
+
+    protected boolean storageAccountExist(final URI storageURI){
+        try {
+            final String storageAccountName = storageURI.getHost().split("\\.")[0];
+            final String containerName = PathUtility.getContainerNameFromUri(storageURI, false);
+            final String blobName = PathUtility.getBlobNameFromURI(storageURI, false);
+
+            List<StorageAccountKey> storageKeys = customTokenCache.getAzureClient().storageAccounts()
+                .getByGroup(testEnv.azureResourceGroup, storageAccountName)
+                .getKeys();
+
+            if (storageKeys.isEmpty()) {
+                return false;
+            } else {
+                String storageAccountKey = storageKeys.get(0).value();
+                CloudStorageAccount account = new CloudStorageAccount(new StorageCredentialsAccountAndKey(storageAccountName, storageAccountKey));
+                CloudBlobClient blobClient = account.createCloudBlobClient();
+                return blobClient.getContainerReference(containerName).getBlockBlobReference(blobName).exists();
+            }
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    protected AzureVMDeploymentInfo createDefaultDeployment(final int numberOfAgents, AzureVMAgentCleanUpTask.DeploymentRegistrar deploymentRegistrar) throws AzureCloudException, IOException {
+        final String templateName = "t" + TestEnvironment.GenerateRandomString(7);
+        final String osType = Constants.OS_TYPE_LINUX;
+        final String initScript = "echo \"" + UUID.randomUUID().toString() + "\"";
+        final String launchMethod = Constants.LAUNCH_METHOD_SSH;
+        final String vmUser = "tstVmUser";
+        final Secret vmPassword = Secret.fromString(UUID.randomUUID().toString());
+
+        StandardUsernamePasswordCredentials vmCredentials = mock(StandardUsernamePasswordCredentials.class);
+        when(vmCredentials.getUsername()).thenReturn(vmUser);
+        when(vmCredentials.getPassword()).thenReturn(vmPassword);
+
+        if (deploymentRegistrar == null) {
+            deploymentRegistrar = mock(AzureVMAgentCleanUpTask.DeploymentRegistrar.class);
+        }
+
+        AzureVMAgentTemplate templateMock = mock(AzureVMAgentTemplate.class);
+        when(templateMock.getResourceGroupName()).thenReturn(testEnv.azureResourceGroup);
+        when(templateMock.getStorageAccountName()).thenReturn(testEnv.azureStorageAccountName);
+        when(templateMock.getLocation()).thenReturn(testEnv.azureLocation);
+        when(templateMock.getTemplateName()).thenReturn(templateName);
+        when(templateMock.getOsType()).thenReturn(osType);
+        when(templateMock.getInitScript()).thenReturn(initScript);
+        when(templateMock.getAgentLaunchMethod()).thenReturn(launchMethod);
+        when(templateMock.getImageReferenceType()).thenReturn("");
+        when(templateMock.getImagePublisher()).thenReturn(testEnv.azureImagePublisher);
+        when(templateMock.getImageOffer()).thenReturn(testEnv.azureImageOffer);
+        when(templateMock.getImageSku()).thenReturn(testEnv.azureImageSku);
+        when(templateMock.getVirtualMachineSize()).thenReturn(testEnv.azureImageSize);
+        when(templateMock.getImage()).thenReturn("");
+        when(templateMock.getVMCredentials()).thenReturn(vmCredentials);
+        when(templateMock.getAzureCloud()).thenReturn(mock(AzureVMCloud.class));
+
+        return AzureVMManagementServiceDelegate.createDeployment(templateMock, numberOfAgents, customTokenCache,deploymentRegistrar);
+    }
+
+    protected VirtualMachine createAzureVM(final String vmName) throws CloudException, IOException{
+        return customTokenCache.getAzureClient().virtualMachines()
+                .define(vmName)
+                .withRegion(testEnv.azureLocation)
+                .withNewResourceGroup(testEnv.azureResourceGroup)
+                .withNewPrimaryNetwork("10.0.0.0/28")
+                .withPrimaryPrivateIpAddressDynamic()
+                .withoutPrimaryPublicIpAddress()
+                .withPopularLinuxImage(KnownLinuxVirtualMachineImage.UBUNTU_SERVER_14_04_LTS)
+                .withRootUsername(TestEnvironment.GenerateRandomString(8))
+                .withRootPassword(TestEnvironment.GenerateRandomString(16) + "AA@@12345") //don't try this at home
+                .create();
+    }
+}
