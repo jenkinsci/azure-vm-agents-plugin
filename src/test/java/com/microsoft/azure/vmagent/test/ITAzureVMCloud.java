97140bd515365fed411d075eb95a495f9f77e032
René Scheibe <rene.scheibe@gmail.com>
2018-10-28 14:55:11 +0100

    Remove unused imports

--- src/test/java/com/microsoft/azure/vmagent/test/ITAzureVMCloud.java
+++ src/test/java/com/microsoft/azure/vmagent/test/ITAzureVMCloud.java
@@ -26,7 +26,6 @@ import hudson.model.Node;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.jenkinsci.plugins.cloudstats.CloudStatistics;
 import org.jenkinsci.plugins.cloudstats.ProvisioningActivity;
 import org.junit.Assert;
 import org.junit.Ignore;
