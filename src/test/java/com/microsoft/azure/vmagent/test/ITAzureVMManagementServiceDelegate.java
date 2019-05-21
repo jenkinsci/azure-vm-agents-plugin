49bb336f60b011603e5f72383d786569365a1c16
Jie Shen <drfish.me@gmail.com>
2019-05-21 16:53:42 +0800

    Fix broken IT (#152)

--- src/test/java/com/microsoft/azure/vmagent/test/ITAzureVMManagementServiceDelegate.java
+++ src/test/java/com/microsoft/azure/vmagent/test/ITAzureVMManagementServiceDelegate.java
@@ -89,6 +89,9 @@ public class ITAzureVMManagementServiceDelegate extends IntegrationTest {
         when(templateMock.getLocation()).thenReturn(testEnv.azureLocation);
         when(templateMock.getInitScript()).thenReturn(writtenData);
         when(templateMock.getStorageAccountType()).thenReturn(SkuName.STANDARD_LRS.toString());
+        AzureVMCloud cloudMock = mock(AzureVMCloud.class);
+        when(cloudMock.getCloudName()).thenReturn("testCloudName");
+        when(templateMock.retrieveAzureCloudReference()).thenReturn(cloudMock);
 
         try {
             delegate.uploadCustomScript(templateMock, uploadFileName);
