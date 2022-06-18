baf4ac45ae9c3b762a5ce107220ad8523e9669ee
Tim Jacomb <timjacomb1+github@gmail.com>
2022-06-18 14:50:27 +0100

    Update maven

--- src/main/resources/scripts/ubuntuInstallMavenScript.sh
+++ src/main/resources/scripts/ubuntuInstallMavenScript.sh
@@ -1,8 +1,8 @@
 # Install Maven
 
-MAVEN_VERSION=3.8.4
+MAVEN_VERSION=3.8.6
 
-sudo curl -O https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+sudo curl -O "https://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
 sudo tar zxvf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt/
 sudo ln -s /opt/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/bin/mvn
 
