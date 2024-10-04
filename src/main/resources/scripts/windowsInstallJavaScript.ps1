65c0fd08fc35ca52cec714a7f5a9995d2cd0473a
George Adams <george.adams@microsoft.com>
2024-10-04 09:29:24 +0100

    java: update installed version to JDK 17
    For Windows I switched from AdoptOpenJDK to Temurin (the new name for the project)
    
    For Ubuntu we will need to change the apt-get install

--- src/main/resources/scripts/windowsInstallJavaScript.ps1
+++ src/main/resources/scripts/windowsInstallJavaScript.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -y adoptopenjdk11
+choco install -y temurin17
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
