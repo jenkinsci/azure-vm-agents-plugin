695bb953b3546d3e3302052a8bb6d3c9004ee344
Tim Jacomb <timjacomb1@gmail.com>
2024-11-09 22:31:14 +0000

    Fixes

--- src/main/java/com/microsoft/azure/vmagent/AzureVMAgentTemplate.java
+++ src/main/java/com/microsoft/azure/vmagent/AzureVMAgentTemplate.java
@@ -44,6 +44,7 @@ import com.microsoft.azure.vmagent.util.Constants;
 import com.microsoft.azure.vmagent.util.FailureStage;
 import com.microsoft.jenkins.credentials.AzureResourceManagerCache;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Extension;
 import hudson.RelativePath;
 import hudson.Util;
@@ -58,7 +59,6 @@ import hudson.security.ACL;
 import hudson.slaves.Cloud;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
-import hudson.slaves.AbstractCloudSlave;
 import hudson.slaves.RetentionStrategy;
 import hudson.util.DescribableList;
 import hudson.util.FormApply;
@@ -389,7 +389,9 @@ public class AzureVMAgentTemplate implements Describable<AzureVMAgentTemplate>, 
 
     private String licenseType;
 
-    private /* lazily initialized */ DescribableList<NodeProperty<?>, NodePropertyDescriptor> nodeProperties;
+    @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "They are serializable in practice")
+    private DescribableList<NodeProperty<?>, NodePropertyDescriptor> nodeProperties =
+            new DescribableList<>(Saveable.NOOP);
 
     // deprecated fields
     private transient boolean isInstallDocker;
@@ -441,7 +443,6 @@ public class AzureVMAgentTemplate implements Describable<AzureVMAgentTemplate>, 
             String agentWorkspace,
             String jvmOptions,
             RetentionStrategy<?> retentionStrategy,
-            List<? extends NodeProperty<?>> nodeProperties,
             boolean executeInitScriptAsRoot,
             boolean doNotUseMachineIfInitFails
     ) {
@@ -498,49 +499,18 @@ public class AzureVMAgentTemplate implements Describable<AzureVMAgentTemplate>, 
         labelDataSet = Label.parse(labels);
 
         this.tags = new ArrayList<>();
-
-        this.nodeProperties = new DescribableList<>(Saveable.NOOP, Util.fixNull(nodeProperties));
     }
 
-    @Deprecated
-    public AzureVMAgentTemplate(
-            String templateName,
-            String templateDesc,
-            String labels,
-            String location,
-            AvailabilityTypeClass availabilityType,
-            String virtualMachineSize,
-            String storageAccountNameReferenceType,
-            String storageAccountType,
-            String newStorageAccountName,
-            String existingStorageAccountName,
-            String diskType,
-            String noOfParallelJobs,
-            Node.Mode usageMode,
-            String osType,
-            String imageTopLevelType,
-            ImageReferenceTypeClass imageReference,
-            AzureComputerLauncher launcher,
-            String initScript,
-            String terminateScript,
-            String credentialsId,
-            String virtualNetworkName,
-            String virtualNetworkResourceGroupName,
-            String subnetName,
-            String nsgName,
-            String agentWorkspace,
-            String jvmOptions,
-            RetentionStrategy<?> retentionStrategy,
-            boolean executeInitScriptAsRoot,
-            boolean doNotUseMachineIfInitFails
-    ) {
-        this(templateName, templateDesc, labels, location, availabilityType, virtualMachineSize,
-            storageAccountNameReferenceType, storageAccountType, newStorageAccountName, existingStorageAccountName,
-            diskType, noOfParallelJobs, usageMode, osType, imageTopLevelType, imageReference, launcher, initScript,
-            terminateScript, credentialsId, virtualNetworkName, virtualNetworkResourceGroupName, subnetName, nsgName,
-            agentWorkspace, jvmOptions, retentionStrategy, null, executeInitScriptAsRoot,
-            doNotUseMachineIfInitFails
-        );
+    public DescribableList<NodeProperty<?>, NodePropertyDescriptor> getNodeProperties() {
+        return nodeProperties;
+    }
+
+    @DataBoundSetter
+    public void setNodeProperties(List<? extends NodeProperty<?>> nodeProperties) throws IOException {
+        if (nodeProperties == null) {
+            this.nodeProperties = new DescribableList<>(Saveable.NOOP);
+        }
+        this.nodeProperties.replaceBy(nodeProperties);
     }
 
     @DataBoundSetter
@@ -876,6 +846,10 @@ public class AzureVMAgentTemplate implements Describable<AzureVMAgentTemplate>, 
 
         if (StringUtils.isBlank(storageAccountType)) {
             storageAccountType = SkuName.STANDARD_LRS.toString();
+        }
+
+        if (nodeProperties == null) {
+            nodeProperties = new DescribableList<>(Saveable.NOOP);
         }
 
         if (StringUtils.isNotBlank(agentLaunchMethod)) {
@@ -1460,10 +1434,6 @@ public class AzureVMAgentTemplate implements Describable<AzureVMAgentTemplate>, 
         this.licenseType = licenseType;
     }
 
-    public DescribableList<NodeProperty<?>, NodePropertyDescriptor> getNodeProperties() {
-        return Objects.requireNonNull(nodeProperties);
-    }
-
     /**
      * Provision new agents using this template.
      *
@@ -1967,8 +1937,9 @@ public class AzureVMAgentTemplate implements Describable<AzureVMAgentTemplate>, 
             return FormValidation.ok();
         }
 
+        // called by stapler
         public List<NodePropertyDescriptor> getNodePropertyDescriptors() {
-            return NodePropertyDescriptor.for_(NodeProperty.all(), AbstractCloudSlave.class);
+            return NodePropertyDescriptor.for_(NodeProperty.all(), AzureVMAgent.class);
         }
 
         @POST
