45194a389c244a8c0346042c67e14ab872801e68
Tim Jacomb <21194782+timja@users.noreply.github.com>
2024-12-18 16:42:19 +0000

    Update maven version in basic image (#600)

--- src/main/resources/scripts/ubuntuInstallMavenScript.sh
+++ src/main/resources/scripts/ubuntuInstallMavenScript.sh
@@ -1,6 +1,6 @@
 # Install Maven
 
-MAVEN_VERSION=3.9.1
+MAVEN_VERSION=3.9.9
 
 sudo curl -O "https://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
 sudo tar zxvf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt/
