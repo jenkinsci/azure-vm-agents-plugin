30e6e2de7d45463e1901124675e8feb3d9ebccf0
Tim Jacomb <timjacomb1+github@gmail.com>
2025-09-15 15:02:26 +0100

    Fix deprecation

--- src/main/java/com/microsoft/azure/vmagent/launcher/AzureComputerLauncher.java
+++ src/main/java/com/microsoft/azure/vmagent/launcher/AzureComputerLauncher.java
@@ -1,9 +1,10 @@
 package com.microsoft.azure.vmagent.launcher;
 
 import hudson.ExtensionPoint;
-import hudson.model.AbstractDescribableImpl;
+import hudson.model.Describable;
+
 import java.io.Serializable;
 
-public abstract class AzureComputerLauncher extends AbstractDescribableImpl<AzureComputerLauncher>
-        implements ExtensionPoint, Serializable {
+public abstract class AzureComputerLauncher
+        implements ExtensionPoint, Serializable, Describable<AzureComputerLauncher> {
 }
